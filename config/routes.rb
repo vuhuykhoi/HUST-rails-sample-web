1ba00afa49f9daa95fffe2dfa5cee74e22f7d7bd
Vu Huy Khoi <vhk.forwork@gmail.com>
2019-03-20 02:43:05 +0000

    devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  devise_for :users
   resources :books
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
